50a35f473ead63ac64213f8f88357c5d6eedfe36
Georg Thiesen <georg.thiesen@rwth-aachen.de>
2026-03-01 16:37:11 +0100

    freezing when game run is loaded could not be solved, however, on linux laptop it is only rarely occuring

--- Scripts/MainMenu.gd
+++ Scripts/MainMenu.gd
@@ -1,15 +1,11 @@
 extends Control
 
 func _ready():
-	print("Start Menu")
-
-func _on_exit_button_button_up():
-	get_tree().quit()
+	pass
 
 func _on_play_button_pressed():
 	print("Play Game!")
-	Global.goto_scene("res://Scenes/GameRun.tscn")
+	#Global.goto_scene("res://Scenes/GameRun.tscn")
 
-func _input(event):
-	if event is InputEventMouseButton:
-		print("Mouse event: ", event.button_index, " pressed: ", event.pressed, " pos: ", event.position, " window focus: ", get_window().has_focus())
+func _on_exit_button_pressed() -> void:
+	get_tree().quit()

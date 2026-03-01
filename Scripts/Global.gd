4906a252c0f00f9ffe42da83b70389e76edfbeec
Ipagaxi <g.thiesen@posteo.de>
2026-03-01 21:32:56 +0100

    fix: fixed freeze on scene switch from menu to loading screen

--- Scripts/Global.gd
+++ Scripts/Global.gd
@@ -25,12 +25,9 @@ func _ready():
 	DisplayServer.window_set_current_screen(target_screen)
 
 func goto_scene(path):
+	_deferred_goto_scene.call_deferred(path)
+	
+func _deferred_goto_scene(path):
 	new_scene_path = path
 	ResourceLoader.load_threaded_request(path)
 	get_tree().change_scene_to_file("res://Scenes/Loading.tscn")
-	
-func _input(event):
-	if event is InputEventMouseButton:
-		print("Mouse event: ", event.button_index, " pressed: ", event.pressed, " pos: ", event.position)
-		print("Input handled: ", event.is_action_pressed("ui_accept"))
-	

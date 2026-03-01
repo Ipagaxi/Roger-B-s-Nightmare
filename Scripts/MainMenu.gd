b9f37b61f05386070307aaeaa9c0873e02fc9c82
Georg Thiesen <georg.thiesen@rwth-aachen.de>
2026-03-01 16:54:45 +0100

    chore: removed .uid entry from .gitignore (used by godot)

--- Scripts/MainMenu.gd
+++ Scripts/MainMenu.gd
@@ -4,8 +4,7 @@ func _ready():
 	pass
 
 func _on_play_button_pressed():
-	print("Play Game!")
-	#Global.goto_scene("res://Scenes/GameRun.tscn")
+	Global.goto_scene("res://Scenes/GameRun.tscn")
 
 func _on_exit_button_pressed() -> void:
 	get_tree().quit()

6c628c5fd589fe54d4868c59f706970d0a295d4b
Georg Thiesen <georg.thiesen@rwth-aachen.de>
2026-03-11 16:47:23 +0100

    feat: menu title is pulsing

--- Scripts/main_menu.gd
+++ Scripts/main_menu.gd
@@ -1,7 +1,13 @@
 extends Control
 
+var sin_vec = Vector2(0.1, 0)
+
 func _ready():
 	pass
+	
+func _process(delta: float) -> void:
+	sin_vec = sin_vec.rotated(asin(delta*3))
+	$UI/TitleSprite.scale = Vector2(4, 4) + Vector2(sin_vec.y, sin_vec.y)
 
 func _on_play_button_pressed():
 	Global.goto_scene("res://Scenes/GameRun.tscn")

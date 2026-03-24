02ae41f809a296290feda86a7848d532a748cafe
Ipagaxi <g.thiesen@posteo.de>
2026-03-24 15:16:04 +0100

    feat: NOW random walk and random npc sprites are added

--- Scripts/npc.gd
+++ Scripts/npc.gd
@@ -11,7 +11,19 @@ var atlas_regions: Array[Rect2] = [
 	Rect2(32.0, 96.0, 32.0, 48.0),
 	Rect2(64.0, 96.0, 32.0, 48.0),
 	Rect2(96.0, 96.0, 32.0, 48.0)
-	]
+]
+	
+var move_dir: Array[Vector2] = [
+	Vector2(0, 0),
+	Vector2(0, 1),
+	Vector2(1, 0),
+	Vector2(1, 1),
+	Vector2(0, -1),
+	Vector2(-1, 0),
+	Vector2(-1, -1)
+]
+
+var move_weights: Array = [600, 1, 1, 1, 1, 1, 1]
 	
 var rng := RandomNumberGenerator.new()
 
@@ -26,4 +38,5 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if Global.game_state == Global.GameState.LOCAL:
-		
+		var end_position = self.global_position + move_dir[rng.rand_weighted(move_weights)] * TilesInterface.TILE_SIZE
+		$MoveOperator.trigger_movement(end_position, self)
